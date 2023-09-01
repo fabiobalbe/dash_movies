e24d3b35073853452e187e496488f77f196db4ee
Fábio Balbé <fabiobalbe@MacBook-Air-de-Fabio.local>
2023-09-01 20:49:15 -0300

    Title

--- index.py
+++ index.py
@@ -5,10 +5,21 @@ from dash.dependencies import Output, Input
 import pandas as pd
 
 font_awesome1 = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css'
-font_awesome2 = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/regular.min.css'
-font_awesome3 = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/solid.min.css'
-
+# font_awesome2 = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/regular.min.css'
+# font_awesome3 = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/solid.min.css'
 meta_tags = [{'name': 'viewport', 'content': 'width=device-width'}]
-external_stylesheets = [font_awesome1, font_awesome2, font_awesome3, meta_tags]
+external_stylesheets = [font_awesome1, meta_tags]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+
+    html.Div([
+        html.Div([
+            html.H5('Movie Data Analyzer', className='title_text')
+        ], className='title_container twelve columns')
+    ], className='row flex_display')
+], className='mainContainer', style={'display': 'flex', 'flex-direction': 'column'})
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
