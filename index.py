1d123517e8497337f68db2563832b2a114a2b8ed
Fábio Balbé <fabiobalbe@MacBook-Air-de-Fabio.local>
2023-09-05 20:59:24 -0300

    Right Collumn Title

--- index.py
+++ index.py
@@ -18,7 +18,28 @@ app.layout = html.Div([
         html.Div([
             html.H5('Movie Data Analyzer', className='title_text')
         ], className='title_container twelve columns')
+    ], className='row flex_display'),
+    
+    html.Div([
+
+        html.Div([
+
+            html.P(dcc.Markdown("""
+                **Below table** shows gross sales **values** for each genre
+                category and **percentage** of share for each genre category
+                in **each year**.
+            """), style={'line-height':'1',
+                         'font-size':'17px',
+                         'text-align':'justify',
+                         'margin-bottom':'20px',
+                         })
+
+        ], className='create_container six columns',
+                 style={'margin-top':'10px',
+                        'margin-bottom':'10px'})
+
     ], className='row flex_display')
+
 ], className='mainContainer', style={'display': 'flex', 'flex-direction': 'column'})
 
 if __name__ == '__main__':
