318ded1efacef608949108cd14e17810d3e09e39
Fábio Balbé <fabiobalbe@MacBook-Air-de-Fabio.local>
2023-09-14 20:28:37 -0300

    Rascunho

--- index.py
+++ index.py
@@ -19,7 +19,7 @@ app.layout = html.Div([
             html.H5('Movie Data Analyzer', className='title_text')
         ], className='title_container twelve columns')
     ], className='row flex_display'),
-    
+
     html.Div([
 
         html.Div([
@@ -28,15 +28,14 @@ app.layout = html.Div([
                 **Below table** shows gross sales **values** for each genre
                 category and **percentage** of share for each genre category
                 in **each year**.
-            """), style={'line-height':'1',
-                         'font-size':'17px',
-                         'text-align':'justify',
-                         'margin-bottom':'20px',
+            """), style={'line-height': '1',
+                         'font-size': '17px',
+                         'text-align': 'justify',
                          })
 
         ], className='create_container six columns',
-                 style={'margin-top':'10px',
-                        'margin-bottom':'10px'})
+            style={'margin-top': '10px',
+                   'margin-bottom': '10px'})
 
     ], className='row flex_display')
 
