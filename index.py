cbab7644e243a4c6ff8e5c367caf1f8f22fc467c
Fábio Balbé <fabiobalbe@MacBook-Air-de-Fabio.local>
2023-09-15 13:59:06 -0300

    Slider

--- index.py
+++ index.py
@@ -10,6 +10,9 @@ font_awesome1 = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/a
 meta_tags = [{'name': 'viewport', 'content': 'width=device-width'}]
 external_stylesheets = [font_awesome1, meta_tags]
 
+df = pd.read_csv('jupyternotebook/update.csv')
+year_list = list(df['Year'].unique())
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
@@ -31,8 +34,29 @@ app.layout = html.Div([
             """), style={'line-height': '1',
                          'font-size': '17px',
                          'text-align': 'justify',
+                         'margin-bottom': '20px'
                          })
 
+        ], className='create_container six columns',
+            style={'margin-top': '10px',
+                   'margin-bottom': '10px'}),
+
+        html.Div([
+            html.Div([
+                html.P('Year:', className='fix_label', style={
+                       'color': 'black', 'margin-top': '15px'}),
+                dcc.Slider(id='select_year',
+                           min=year_list[0],
+                           max=year_list[-1],
+                           value=(year_list[0]+year_list[-1])/2,
+                           step=1,
+                           included=False,
+                           updatemode='drag',
+                           tooltip={'always_visible': False},
+                           marks={str(yrs): str(yrs) for yrs in range(
+                               year_list[0], year_list[-1], 2)},
+                           className='slider_component')
+            ], className='container_slider')
         ], className='create_container six columns',
             style={'margin-top': '10px',
                    'margin-bottom': '10px'})
